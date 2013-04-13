21b02b6cf0f203bccee939ef6af36d29c97bb7b7
Debjani Mitra <dm26@cs.brown.edu>
2013-04-13 17:25:46 -0400

    Basic GUI for 1st milestone.

---
+++ src/NotificationsPanel.java
@@ -0,0 +1,20 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class NotificationsPanel extends JPanel {
+
+	public NotificationsPanel(){
+		super();
+		this.setBackground(new Color(236,234,224));
+		this.setSize(new Dimension(600, 600));
+		this.setPreferredSize(new Dimension(550, 600));
+		JLabel openingLabel=new JLabel("                                            Welcome to Your Notifications");
+		this.setLayout(new BorderLayout());
+		this.add(openingLabel, BorderLayout.CENTER);
+	}
+}
